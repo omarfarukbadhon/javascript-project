9ec3c36839bb477b344db59a571687741b348040
omarfarukbadhon <omarfarukbadhon@gmail.com>
2024-02-15 09:28:43 +0600

    shopkepper project added

---
+++ return from shopkepper after buying product/practice.js
@@ -0,0 +1,47 @@
+const prompt = require("prompt-sync")();
+
+// Task One
+
+// var orangePrice = prompt("What is the price of Orange: ");
+// var applePrice = prompt("What is the price of Apple: ");
+
+// var returnPrice = 1000- (parseInt(orangePrice) + parseInt(applePrice));
+
+let score = 75;
+let grade;
+if (score >= 90) {
+  grade = "A";
+} else if (score >= 80) {
+  grade = "B";
+}
+console.log(grade);
+
+// // Task Two
+
+// var banglaMarks = 85;
+// var mathMark = 100;
+// var chemistryMark = 97;
+// var physicsMark = 94;
+// var biologyMark = 81;
+
+// var totalMark = banglaMarks + mathMark + chemistryMark + physicsMark + biologyMark;
+
+// var averageMark = totalMark / 5;
+
+// console.log(averageMark.toFixed(2));
+
+// // Task Three
+
+// var targetValue = 74;
+
+// var reminder = targetValue%5;
+
+// console.log(reminder);
+
+// // Task Four
+
+// var a = isNaN('gdshgehet');
+// var a = isNaN('20');
+// var a = isNaN(20);
+
+// console.log(a);
